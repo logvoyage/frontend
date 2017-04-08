585cf4f50e5d888a4301602cd91dfd36bae84870
Andrew <firstrow@gmail.com>
2017-04-09 01:06:46 +0300

    Added test route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test.component';
+
+const appRoutes: Routes = [
+  { path: 'test', component: TestComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
   ],
   imports: [
+    MaterialModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/test.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: `
+    test module
+  `
+})
+export class TestComponent { }
